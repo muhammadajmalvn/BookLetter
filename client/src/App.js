5d9c9f85c0f14ea4e2cc03dbe3f0a095a9d8fac2
muhammadajmalvn <muhammadajmalvn@gmail.com>
2023-03-18 02:32:47 +0530

    frontend route started

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Signup from './Components/User/Signup'
+import Login from './Components/User/Login'
+import Homepage from './Components/User/Homepage'
+import UserProfile from './Components/User/UserProfile'
+import AdminLogin from './Components/Admin/AdminLogin'
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' exact Component={Homepage} />
+          <Route path='/signup' Component={Signup}/>
+          <Route path='/login' Component={Login}/>
+          <Route path='/profile' exact Component={UserProfile}/>
+          <Route path='/admin' exact Component={AdminLogin}/>
+
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
+
